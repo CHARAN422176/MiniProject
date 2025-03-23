d81d31ea9b8d390a2bb32c6d7db0be7ad51017cf
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-03-23 21:32:24 +0530

    commit on "main"

--- models/xbert.py
+++ models/xbert.py
@@ -867,7 +867,7 @@ class BertModel(BertPreTrainedModel):
 
     @add_start_docstrings_to_model_forward(BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     @add_code_sample_docstrings(
-        tokenizer_class=_TOKENIZER_FOR_DOC,
+        # tokenizer_class=_TOKENIZER_FOR_DOC,
         checkpoint="bert-base-uncased",
         output_type=BaseModelOutputWithPoolingAndCrossAttentions,
         config_class=_CONFIG_FOR_DOC,

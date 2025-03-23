979d9884fac2d34b912adf0c0d28218b29189b8e
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-03-23 21:34:37 +0530

    commit on "main"

--- models/xbert.py
+++ models/xbert.py
@@ -1357,7 +1357,7 @@ class BertForMaskedLM(BertPreTrainedModel):
 
     @add_start_docstrings_to_model_forward(BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     @add_code_sample_docstrings(
-        tokenizer_class=_TOKENIZER_FOR_DOC,
+        # tokenizer_class=_TOKENIZER_FOR_DOC,
         checkpoint="bert-base-uncased",
         output_type=MaskedLMOutput,
         config_class=_CONFIG_FOR_DOC,
